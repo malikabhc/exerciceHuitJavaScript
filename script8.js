5957baca9bb5230d8224797c77a17c6e88fe043a
Malikabhc <malika.bouhcine@gmail.com>
2018-06-22 10:56:17 +0200

    correction

--- script8.js
+++ script8.js
@@ -4,9 +4,13 @@ function calculAge() {
   var age = document.getElementById('age').value;
   var regex = /^[\d]+$/;
   //Boite de dialogue avec appel des variables
-  if(regex.test(age) && (age >= 18)){
-    alert('Vous êtes majeur');
-    } else {
-    alert('Vous êtes mineur');x
-    }
+  if(regex.test(age) && age < 130) {
+    if(age >= 18){
+      alert('Vous êtes majeur');
+      } else {
+      alert('Vous êtes mineur');
+      }
+  } else {
+    alert('Age invalide');
+  }
 }
