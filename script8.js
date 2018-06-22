52f2daff0f533f37cf4a6a8050338e4ab8d994de
Malikabhc <malika.bouhcine@gmail.com>
2018-06-22 09:14:34 +0200

    exercice8

---
+++ script8.js
@@ -0,0 +1,12 @@
+// Déclaration de la fonction
+function calculAge() {
+  //Déclaration des variables
+  var age = document.getElementById('age').value;
+  var regex = /^[\d]+$/;
+  //Boite de dialogue avec appel des variables
+  if(regex.test(age) && (age >= 18)){
+    alert('Vous êtes majeur');
+    } else {
+    alert('Vous êtes mineur');x
+    }
+}
